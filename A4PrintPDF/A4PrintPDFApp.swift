54eebd26709725d57db2dd902a6e832715a39788
ゆー <unisix@Mac.local>
2026-02-07 17:58:20 +0900

    Initial Commit

---
+++ A4PrintPDF/A4PrintPDFApp.swift
@@ -0,0 +1,17 @@
+//
+//  A4PrintPDFApp.swift
+//  A4PrintPDF
+//
+//  Created by ゆー on 2026/02/07.
+//
+
+import SwiftUI
+
+@main
+struct A4PrintPDFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ A4PrintPDF/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  A4PrintPDF
+//
+//  Created by ゆー on 2026/02/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
